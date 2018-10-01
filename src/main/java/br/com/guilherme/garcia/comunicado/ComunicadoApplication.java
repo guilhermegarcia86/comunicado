f0716edad979dc8bc2a5b4335380424f49dd397e
guilh <guilherme.garcia86@gmail.com>
2018-10-01 20:08:11 -0300

    Retirado config server

--- src/main/java/br/com/guilherme/garcia/comunicado/ComunicadoApplication.java
+++ src/main/java/br/com/guilherme/garcia/comunicado/ComunicadoApplication.java
@@ -2,12 +2,12 @@ package br.com.guilherme.garcia.comunicado;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
-@EnableEurekaClient
+@EnableFeignClients("br.com.guilherme.garcia.comunicado")
+@EnableDiscoveryClient
 public class ComunicadoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/guilherme/garcia/comunicado/client/ComunicadoClient.java
+++ src/main/java/br/com/guilherme/garcia/comunicado/client/ComunicadoClient.java
@@ -2,12 +2,14 @@ package br.com.guilherme.garcia.comunicado.client;
 
 import java.util.List;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import br.com.guilherme.garcia.comunicado.dto.Comunicado;
 
 @FeignClient(name="crud-service")
+@RibbonClient(name="crud-service")
 public interface ComunicadoClient {
 	
 	@GetMapping("/comunicado/all")
