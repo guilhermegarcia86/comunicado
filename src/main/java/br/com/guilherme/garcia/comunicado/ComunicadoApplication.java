85243244bf2e8ff86ae9ef5e32b9f710276dfaf8
Guilherme Garcia <guilherme.garcia86@gmail.com>
2018-10-14 20:24:21 -0300

    Add cloud foundry config

--- src/main/java/br/com/guilherme/garcia/comunicado/ComunicadoApplication.java
+++ src/main/java/br/com/guilherme/garcia/comunicado/ComunicadoApplication.java
@@ -3,14 +3,22 @@ package br.com.guilherme.garcia.comunicado;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableFeignClients("br.com.guilherme.garcia.comunicado")
+//@EnableFeignClients("br.com.guilherme.garcia.comunicado")
 @EnableDiscoveryClient
 public class ComunicadoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ComunicadoApplication.class, args);
+	}
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 }
--- src/main/java/br/com/guilherme/garcia/comunicado/client/ComunicadoClient.java
+++ src/main/java/br/com/guilherme/garcia/comunicado/client/ComunicadoClient.java
@@ -1,18 +1,10 @@
 package br.com.guilherme.garcia.comunicado.client;
 
-import java.util.List;
-
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import br.com.guilherme.garcia.comunicado.dto.Comunicado;
-
-@FeignClient(name="zuul-api-gateway-server")
-@RibbonClient(name="crud-service")
+//@FeignClient(name="zuul-api-gateway-server")
+//@RibbonClient(name="crud-service")
 public interface ComunicadoClient {
 	
-	@GetMapping("/crud-service/comunicado/all")
-	public List<Comunicado> getAllComunicados();
+//	@GetMapping("/crud-service/comunicado/all")
+//	public List<Comunicado> getAllComunicados();
 
 }
