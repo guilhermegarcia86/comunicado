86b45745307be834f3d48d1b613f1959410742aa
Guilherme Garcia <guilherme.garcia86@gmail.com>
2018-10-18 20:45:13 -0300

    Add feign clients

--- src/main/java/br/com/guilherme/garcia/comunicado/controller/ComunicadoController.java
+++ src/main/java/br/com/guilherme/garcia/comunicado/controller/ComunicadoController.java
@@ -1,35 +1,23 @@
 package br.com.guilherme.garcia.comunicado.controller;
 
-import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.guilherme.garcia.comunicado.client.ComunicadoClient;
 import br.com.guilherme.garcia.comunicado.dto.Comunicado;
+import br.com.guilherme.garcia.comunicado.service.ComunicadoService;
 
 @RestController
 public class ComunicadoController {
 	
-	private Logger logger = LoggerFactory.getLogger(this.getClass());
-	
 	@Autowired
-	private ComunicadoClient comunicadoClient;
+	private ComunicadoService comunicadoService;
 	
-	@GetMapping("/feign/all")
-	public List<Comunicado> getAll(){
-		logger.info("Realizara uma chamada para outro microservice");
-		return comunicadoClient.getAllComunicados();
-	}
-	
-	@PostMapping("file")
-	public Comunicado info(@RequestBody Comunicado comunicado) {
-		return comunicado;
+	@PostMapping("add")
+	public ResponseEntity<Comunicado> add(@RequestBody Comunicado comunicado) {
+		return ResponseEntity.ok(comunicadoService.add(comunicado));
 	}
 
 }
